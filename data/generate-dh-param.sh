7e5860e6ed40ff4f518eac6325d47868c349f7c8
Daniel, Kim Hangon <hangon1234@gmail.com>
2021-02-28 02:40:42 +0900

    First commit

---
+++ data/generate-dh-param.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ -f "/etc/ssl/ffdhe4096.pem" ]; then
+	echo "/etc/ssl/ffdhe4096.pem exists"
+else
+	echo "/etc/ssl/ffdhe4096.pem is missing"
+	echo "Create Diffie–Hellman parameters"
+	openssl dhparam -out /etc/ssl/ffdhe4096.pem 4096
+fi
