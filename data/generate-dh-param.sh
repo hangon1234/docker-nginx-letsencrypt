bc831fd93b0ad671bcf316aa17e8dfe94147cd25
Daniel, Kim Hangon <hangon1234@gmail.com>
2021-02-28 14:32:23 +0900

    change bug

--- data/generate-dh-param.sh
+++ data/generate-dh-param.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if [ -f "/etc/ssl/ffdhe4096.pem" ]; then
-	echo "/etc/ssl/ffdhe4096.pem exists"
+if [ -f "/etc/ssl/ffdhe2048.pem" ]; then
+	echo "/etc/ssl/ffdhe2048.pem exists"
 else
-	echo "/etc/ssl/ffdhe4096.pem is missing"
+	echo "/etc/ssl/ffdhe2048.pem is missing"
 	echo "Create Diffie–Hellman parameters"
-	openssl dhparam -out /etc/ssl/ffdhe4096.pem 2048
+	openssl dhparam -out /etc/ssl/ffdhe2048.pem 2048
 fi
